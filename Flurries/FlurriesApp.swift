c19a4fe4070a1d41ac380711b5a6a0476440e745
Patrick Serrano <hello@patrickserrano.com>
2022-02-18 20:53:19 -0500

    Initial Commit

---
+++ Flurries/FlurriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlurriesApp.swift
+//  Flurries
+//
+//  Created by Patrick Serrano on 2/18/22.
+//
+
+import SwiftUI
+
+@main
+struct FlurriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
